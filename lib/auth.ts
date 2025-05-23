995dae682abff56da7041cee7a2b02df4709d9db
xxgw1997 <540040588@qq.com>
2025-05-23 09:12:52 +0800

    fix: bug

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,6 @@
 import { DrizzleAdapter } from '@auth/drizzle-adapter'
 import NextAuth from 'next-auth'
 import Google from 'next-auth/providers/google'
-import GitHub from 'next-auth/providers/github'
 
 import { createDb } from '@/lib/db'
 
@@ -18,7 +17,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth(() => {
       sessionsTable: sessions,
       verificationTokensTable: verificationTokens
     }),
-    providers: [Google, GitHub({ clientId: process.env.GITHUB_ID, clientSecret: process.env.GITHUB_SECRET })],
+    providers: [Google],
     session: {
       strategy: 'jwt'
     }
